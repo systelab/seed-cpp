95f4c7f3e23528b7c36ec2c083a8bd4858a10a13
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2018-03-19 16:39:20 +0100

    Moved template definition of EntityMgr to header file.

--- src/SeedCppCore/Model/EntityMgr.h
+++ src/SeedCppCore/Model/EntityMgr.h
@@ -10,20 +10,90 @@ namespace seed_cpp { namespace model {
 	class EntityMgr
 	{
 	public:
-		EntityMgr();
-		EntityMgr(const EntityMgr<Entity>&);
-		virtual ~EntityMgr();
+		EntityMgr()
+			:m_entities()
+		{
+		}
 
-		unsigned int count() const;
+		EntityMgr(const EntityMgr<Entity>& other)
+			:m_entities(other.m_entities)
+		{
+		}
 
-		const Entity* getEntity(unsigned int index) const;
-		const Entity* getEntityById(unsigned int id) const;
+		virtual ~EntityMgr<Entity>()
+		{
+		}
 
-		const Entity& addEntity(std::unique_ptr<Entity>);
-		const Entity& editEntity(std::unique_ptr<Entity>);
-		void deleteEntity(unsigned int id);
+		unsigned int count() const
+		{
+			return m_entities.size();
+		}
 
-		void setEntities(std::vector<std::unique_ptr<Entity> >);
+		const Entity* getEntity(unsigned int index) const
+		{
+			return m_entities[index].get();
+		}
+
+		const Entity* getEntityById(unsigned int id) const
+		{
+			auto it = std::find_if(m_entities.begin(), m_entities.end(),
+				[id](const std::unique_ptr<Entity>& entity)
+				{
+					return entity->getId() == id;
+				});
+			return ((it != m_entities.end()) ? it->get() : NULL);
+		}
+
+		const Entity& addEntity(std::unique_ptr<Entity>)
+		{
+			m_entities.push_back(std::move(entity));
+			return *m_entities.back();
+		}
+
+		const Entity& editEntity(std::unique_ptr<Entity>)
+		{
+			if (!entity->getId())
+			{
+				throw std::exception("EntityMgr<Entity>::editEntity(): Trying to edit an entity without id");
+			}
+
+			unsigned int id = *entity->getId();
+			for (auto itr = m_entities.begin(); itr != m_entities.end(); itr++)
+			{
+				if ((*itr)->getId() == id)
+				{
+					itr->swap(entity);
+					return **itr;
+				}
+			}
+
+			throw std::exception("EntityMgr<Entity>::deleteEntity: Trying to edit an entity that it's not on manager");
+		}
+
+		void deleteEntity(unsigned int id)
+		{
+			auto it = std::find_if(m_entities.begin(), m_entities.end(),
+				[id](const std::unique_ptr<Entity>& entity)
+				{
+					return entity->getId() == id;
+				});
+
+			if (it != m_entities.end())
+			{
+				std::unique_ptr<Entity> entity;
+				it->swap(entity);
+				m_entities.erase(it);
+			}
+			else
+			{
+				throw std::exception("EntityMgr<Entity>::deleteEntity Entity id not found");
+			}
+		}
+
+		void setEntities(std::vector<std::unique_ptr<Entity> >)
+		{
+			m_entities = std::move(entities);
+		}
 
 	private:
 		std::vector<std::unique_ptr<Entity> > m_entities;
