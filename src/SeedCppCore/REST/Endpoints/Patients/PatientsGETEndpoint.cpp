12b05147c34e5b1e1d6f9eba5f800abb8feb68aa
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2018-03-27 16:43:37 +0200

    Implemented endpoint to GET a single patient

--- src/SeedCppCore/REST/Endpoints/Patients/PatientsGETEndpoint.cpp
+++ src/SeedCppCore/REST/Endpoints/Patients/PatientsGETEndpoint.cpp
@@ -1,7 +1,12 @@
 #include "StdAfx.h"
 #include "PatientsGETEndpoint.h"
 
+#include "DAL/Translators/JSON/IJSONSaveTranslator.h"
+#include "DAL/Translators/JSON/IJSONTranslatorsFactory.h"
 #include "Model/Patient.h"
+
+#include "JSONAdapterInterface/IJSONAdapter.h"
+#include "JSONAdapterInterface/IJSONDocument.h"
 
 #include "WebServerInterface/Model/Reply.h"
 
@@ -9,9 +14,13 @@
 namespace seed_cpp { namespace rest {
 
 	PatientsGETEndpoint::PatientsGETEndpoint(unsigned int id,
-											 model::EntityMgr<model::Patient>& patientMgr)
+											 model::EntityMgr<model::Patient>& patientMgr,
+											 dal::IJSONTranslatorsFactory& jsonTranslatorsFactory,
+											 systelab::json_adapter::IJSONAdapter& jsonAdapter)
 		:m_id(id)
 		,m_patientMgr(patientMgr)
+		,m_jsonTranslatorsFactory(jsonTranslatorsFactory)
+		,m_jsonAdapter(jsonAdapter)
 	{
 	}
 	
@@ -21,9 +30,25 @@ namespace seed_cpp { namespace rest {
 
 	std::unique_ptr<systelab::web_server::Reply> PatientsGETEndpoint::execute()
 	{
+		const model::Patient* patient = m_patientMgr.getEntityById(m_id);
+		if (!patient)
+		{
+			auto reply = std::make_unique<systelab::web_server::Reply>();
+			reply->setStatus(systelab::web_server::Reply::NOT_FOUND);
+			return reply;
+		}
+
+		auto jsonDocument = m_jsonAdapter.buildEmptyDocument();
+		systelab::json_adapter::IJSONValue& jsonRoot = jsonDocument->getRootValue();
+		jsonRoot.setType(systelab::json_adapter::OBJECT_TYPE);
+
+		auto patientTranslator = m_jsonTranslatorsFactory.buildPatientSaveTranslator(*patient);
+		patientTranslator->saveEntityToJSON(jsonRoot);
+
 		auto reply = std::make_unique<systelab::web_server::Reply>();
 		reply->setStatus(systelab::web_server::Reply::OK);
-		reply->setContent("PATATA!");
+		reply->setContent(jsonDocument->serialize());
+
 		return reply;
 	}
 
--- src/SeedCppCore/REST/Endpoints/Patients/PatientsGETEndpoint.h
+++ src/SeedCppCore/REST/Endpoints/Patients/PatientsGETEndpoint.h
@@ -4,8 +4,17 @@
 #include "REST/Endpoints/IEndpoint.h"
 
 
-namespace seed_cpp { namespace model {
-	class Patient;
+namespace seed_cpp {
+	namespace dal {
+		class IJSONTranslatorsFactory;
+	}
+	namespace model {
+		class Patient;
+	}
+}
+
+namespace systelab { namespace json_adapter {
+	class IJSONAdapter;
 }}
 
 namespace seed_cpp { namespace rest {
@@ -15,14 +24,19 @@ namespace seed_cpp { namespace rest {
 	class PatientsGETEndpoint : public IEndpoint
 	{
 	public:
-		PatientsGETEndpoint(unsigned int id, model::EntityMgr<model::Patient>&);
+		PatientsGETEndpoint(unsigned int id,
+							model::EntityMgr<model::Patient>&,
+							dal::IJSONTranslatorsFactory&,
+							systelab::json_adapter::IJSONAdapter&);
 		virtual ~PatientsGETEndpoint();
 
 		std::unique_ptr<systelab::web_server::Reply> execute();
 
 	private:
-		model::EntityMgr<model::Patient>& m_patientMgr;
 		unsigned int m_id;
+		model::EntityMgr<model::Patient>& m_patientMgr;
+		dal::IJSONTranslatorsFactory& m_jsonTranslatorsFactory;
+		systelab::json_adapter::IJSONAdapter& m_jsonAdapter;
 	};
 
 }}
