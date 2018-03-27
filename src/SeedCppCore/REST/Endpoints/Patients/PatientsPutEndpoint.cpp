cdcb79ef9552041c6c8097566dc13bc298d7031e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2018-03-27 20:35:13 +0200

    Added new patients PUT endpoint

---
+++ src/SeedCppCore/REST/Endpoints/Patients/PatientsPutEndpoint.cpp
@@ -0,0 +1,92 @@
+#include "StdAfx.h"
+#include "PatientsPutEndpoint.h"
+
+#include "DAL/DAO/Db/IDbDAOFactory.h"
+#include "DAL/DAO/ISaveDAO.h"
+#include "DAL/Translators/JSON/IJSONLoadTranslator.h"
+#include "DAL/Translators/JSON/IJSONSaveTranslator.h"
+#include "DAL/Translators/JSON/IJSONTranslatorsFactory.h"
+#include "Model/Patient.h"
+#include "REST/Helpers/ReplyBuilderHelper.h"
+
+#include "JSONAdapterInterface/IJSONAdapter.h"
+#include "JSONAdapterInterface/IJSONDocument.h"
+
+#include "WebServerInterface/Model/Reply.h"
+
+
+namespace seed_cpp { namespace rest {
+
+	PatientsPutEndpoint::PatientsPutEndpoint(unsigned int patientId,
+											 const std::string& requestContent,
+											 model::EntityMgr<model::Patient>& patientMgr,
+											 dal::IDbDAOFactory& dbDAOFactory,
+											 dal::IJSONTranslatorsFactory& jsonTranslatorsFactory,
+											 systelab::json_adapter::IJSONAdapter& jsonAdapter)
+		:m_patientId(patientId)
+		,m_requestContent(requestContent)
+		,m_patientMgr(patientMgr)
+		,m_dbDAOFactory(dbDAOFactory)
+		,m_jsonTranslatorsFactory(jsonTranslatorsFactory)
+		,m_jsonAdapter(jsonAdapter)
+	{
+	}
+	
+	PatientsPutEndpoint::~PatientsPutEndpoint()
+	{
+	}
+
+	std::unique_ptr<systelab::web_server::Reply> PatientsPutEndpoint::execute()
+	{
+		std::unique_ptr<systelab::json_adapter::IJSONDocument> jsonRequest = m_jsonAdapter.buildDocumentFromString(m_requestContent);
+		if (!jsonRequest)
+		{
+			return ReplyBuilderHelper::build(systelab::web_server::Reply::BAD_REQUEST);
+		}
+
+		// TBD: Validate schema
+
+		try
+		{
+			const model::Patient* existingPatient = m_patientMgr.getEntityById(m_patientId);
+			if (existingPatient)
+			{
+				auto patientToUpdate = std::make_unique<model::Patient>(*existingPatient);
+				auto patientJSONLoadTranslator = m_jsonTranslatorsFactory.buildPatientLoadTranslator(*patientToUpdate);
+				patientJSONLoadTranslator->loadEntityFromJSON(jsonRequest->getRootValue());
+
+				auto patientSaveDAO = m_dbDAOFactory.buildPatientSaveDAO(*patientToUpdate);
+				patientSaveDAO->updateEntity();
+				const model::Patient& updatedPatient = m_patientMgr.addEntity(std::move(patientToUpdate));
+
+				auto jsonResponse = m_jsonAdapter.buildEmptyDocument();
+				auto patientJSONSaveTranslator = m_jsonTranslatorsFactory.buildPatientSaveTranslator(updatedPatient);
+				patientJSONSaveTranslator->saveEntityToJSON(jsonResponse->getRootValue());
+
+				return ReplyBuilderHelper::build(systelab::web_server::Reply::OK, jsonResponse->serialize());
+			}
+			else
+			{
+				auto patientToAdd = std::make_unique<model::Patient>();
+				auto patientJSONLoadTranslator = m_jsonTranslatorsFactory.buildPatientLoadTranslator(*patientToAdd);
+				patientJSONLoadTranslator->loadEntityFromJSON(jsonRequest->getRootValue());
+
+				auto patientSaveDAO = m_dbDAOFactory.buildPatientSaveDAO(*patientToAdd);
+				patientSaveDAO->addEntity();
+				const model::Patient& addedPatient = m_patientMgr.addEntity(std::move(patientToAdd));
+
+				auto jsonResponse = m_jsonAdapter.buildEmptyDocument();
+				auto patientJSONSaveTranslator = m_jsonTranslatorsFactory.buildPatientSaveTranslator(addedPatient);
+				patientJSONSaveTranslator->saveEntityToJSON(jsonResponse->getRootValue());
+
+				return ReplyBuilderHelper::build(systelab::web_server::Reply::CREATED, jsonResponse->serialize());
+			}
+		}
+		catch (std::exception& exc)
+		{
+			return ReplyBuilderHelper::build(systelab::web_server::Reply::INTERNAL_SERVER_ERROR, exc.what());
+		}
+	}
+
+}}
+
---
+++ src/SeedCppCore/REST/Endpoints/Patients/PatientsPutEndpoint.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Model/EntityMgr.h"
+#include "REST/Endpoints/IEndpoint.h"
+
+
+namespace seed_cpp {
+	namespace dal {
+		class IDbDAOFactory;
+		class IJSONTranslatorsFactory;
+	}
+	namespace model {
+		class Patient;
+	}
+}
+
+namespace systelab { namespace json_adapter {
+	class IJSONAdapter;
+}}
+
+namespace seed_cpp { namespace rest {
+
+	class IEndpoint;
+
+	class PatientsPutEndpoint : public IEndpoint
+	{
+	public:
+		PatientsPutEndpoint(unsigned int patientId,
+							const std::string& requestContent,
+							model::EntityMgr<model::Patient>&,
+							dal::IDbDAOFactory&,
+							dal::IJSONTranslatorsFactory&,
+							systelab::json_adapter::IJSONAdapter&);
+		virtual ~PatientsPutEndpoint();
+
+		std::unique_ptr<systelab::web_server::Reply> execute();
+
+	private:
+		unsigned int m_patientId;
+		std::string m_requestContent;
+		model::EntityMgr<model::Patient>& m_patientMgr;
+		dal::IDbDAOFactory& m_dbDAOFactory;
+		dal::IJSONTranslatorsFactory& m_jsonTranslatorsFactory;
+		systelab::json_adapter::IJSONAdapter& m_jsonAdapter;
+	};
+
+}}
+
