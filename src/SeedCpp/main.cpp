6d2f19190a0c1afba82129db3fe0cd98311a74d3
Alfons Serra <alfonsoserra@gmail.com>
2019-03-30 08:38:21 +0100

    Setup port and CORS

--- src/SeedCpp/main.cpp
+++ src/SeedCpp/main.cpp
@@ -56,36 +56,42 @@ std::unique_ptr<systelab::db::IDatabase> loadDatabase()
 	return database;
 }
 
-std::unique_ptr<systelab::web_server::IServer> loadWebServer()
+std::unique_ptr<systelab::web_server::IServer> loadWebServer(int port, bool enableCors)
 {
 	systelab::web_server::Configuration configuration;
 	configuration.setHostAddress("127.0.0.1");
-	configuration.setPort(8080);
+	configuration.setPort(port);
 	configuration.setThreadPoolSize(5);
 
 	systelab::web_server::CORSConfiguration &corsConfiguration = configuration.getCORSConfiguration();
-	corsConfiguration.setEnabled(true);
-	corsConfiguration.addAllowedOrigin("*");
-	corsConfiguration.addAllowedHeader("origin");
-	corsConfiguration.addAllowedHeader("content-type");
-	corsConfiguration.addAllowedHeader("accept");
-	corsConfiguration.addAllowedHeader("authorization");
-	corsConfiguration.addAllowedHeader("Etag");
-	corsConfiguration.addAllowedHeader("if-none-match");
-	corsConfiguration.setAllowedCredentials(true);
-	corsConfiguration.addAllowedMethod("GET");
-	corsConfiguration.addAllowedMethod("POST");
-	corsConfiguration.addAllowedMethod("PUT");
-	corsConfiguration.addAllowedMethod("DELETE");
-	corsConfiguration.addAllowedMethod("OPTIONS");
-	corsConfiguration.addAllowedMethod("HEAD");
-	corsConfiguration.setMaxAge(1209600);
-	corsConfiguration.addExposedHeader("origin");
-	corsConfiguration.addExposedHeader("content-type");
-	corsConfiguration.addExposedHeader("accept");
-	corsConfiguration.addExposedHeader("authorization");
-	corsConfiguration.addExposedHeader("ETag");
-	corsConfiguration.addExposedHeader("if-none-match");
+	if (enableCors) {
+		corsConfiguration.setEnabled(true);
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("origin");
+		corsConfiguration.addAllowedHeader("content-type");
+		corsConfiguration.addAllowedHeader("accept");
+		corsConfiguration.addAllowedHeader("authorization");
+		corsConfiguration.addAllowedHeader("Etag");
+		corsConfiguration.addAllowedHeader("if-none-match");
+		corsConfiguration.setAllowedCredentials(true);
+		corsConfiguration.addAllowedMethod("GET");
+		corsConfiguration.addAllowedMethod("POST");
+		corsConfiguration.addAllowedMethod("PUT");
+		corsConfiguration.addAllowedMethod("DELETE");
+		corsConfiguration.addAllowedMethod("OPTIONS");
+		corsConfiguration.addAllowedMethod("HEAD");
+		corsConfiguration.setMaxAge(1209600);
+		corsConfiguration.addExposedHeader("origin");
+		corsConfiguration.addExposedHeader("content-type");
+		corsConfiguration.addExposedHeader("accept");
+		corsConfiguration.addExposedHeader("authorization");
+		corsConfiguration.addExposedHeader("ETag");
+		corsConfiguration.addExposedHeader("if-none-match");
+	}
+	else
+	{
+		corsConfiguration.setEnabled(false);
+	}
 
 	systelab::web_server::boostasio::ServerFactory serverFactory;
 	return serverFactory.buildServer(configuration);
@@ -96,19 +102,46 @@ std::unique_ptr<systelab::json::IJSONAdapter> loadJSONAdapter()
 	return std::make_unique<systelab::json::rapidjson::JSONAdapter>();
 }
 
-
-int main()
+int main(int argc, char** argv)
 {
+	int port = 8080;
+	bool enableCors = false;
+
+	int opt; 
+	while((opt = getopt(argc, argv, ":p:cx")) != -1)  
+	{
+		switch(opt)  
+        	{
+			case 'p':
+				if (atoi(optarg)>0)
+				{  
+					port=atoi(optarg);
+					std::cout << "port is set to: " << port << std::endl; 
+				}
+                		break;  
+			case 'c':
+				std::cout << "CORS is enabled" << std::endl;  
+				enableCors = true;
+				break;
+			case ':':
+				std::cout << "Option -" << optopt << " needs a value" << std::endl;
+				break;
+			case '?':
+				std::cout << "Unknown option: -" << optopt << std::endl;
+				break;
+		}
+	}  
+
 	try
 	{
 		std::unique_ptr<systelab::db::IDatabase> database = loadDatabase();
-		std::unique_ptr<systelab::web_server::IServer> webServer = loadWebServer();
+		std::unique_ptr<systelab::web_server::IServer> webServer = loadWebServer(port,enableCors);
 		std::unique_ptr<systelab::json::IJSONAdapter> jsonAdapter = loadJSONAdapter();
 
 		seed_cpp::Core core(std::move(database), std::move(webServer), std::move(jsonAdapter));
 		core.execute();
 
-		std::cout << "Seed core is now running..." << std::endl;
+		std::cout << "Seed core is now running at " << port << " ..." << std::endl;
 		while (true)
 		{
 		}
