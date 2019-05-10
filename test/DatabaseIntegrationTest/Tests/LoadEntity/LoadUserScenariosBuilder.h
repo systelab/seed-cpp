af27aba5048f374b6f67f2b00bc813ad0b218508
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-05-10 18:36:38 +0200

    Added scenarios for users load

---
+++ test/DatabaseIntegrationTest/Tests/LoadEntity/LoadUserScenariosBuilder.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Tests/LoadEntity/LoadEntityTestData.h"
+
+
+namespace seed_cpp { namespace db_test {
+
+	class LoadUserScenariosBuilder
+	{
+	public:
+		static std::vector<LoadEntityTestData> build();
+	};
+
+}}
