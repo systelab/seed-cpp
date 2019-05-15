9d86bfffcab9ce06cc1e08839d92245ddef5e8e3
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-05-15 07:48:56 +0200

    Added SQL executor

---
+++ test/DatabaseIntegrationTest/Tools/SQLExecutor.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "SQLExecutor.h"
+
+#include <fstream>
+#include <sqlite3.h>
+
+
+namespace seed_cpp { namespace db_test {
+
+	SQLExecutor::SQLExecutor(const std::string& dbFilepath)
+		:m_dbFilepath(dbFilepath)
+	{
+	}
+
+	SQLExecutor::~SQLExecutor() = default;
+
+	bool SQLExecutor::executeScripts(const std::vector<std::string>& scripts)
+	{
+		for (const auto& script : scripts)
+		{
+			if (!executeScript(script))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	bool SQLExecutor::executeScript(const std::string& scriptFilepath)
+	{
+		std::ifstream ifs(scriptFilepath);
+		if (!ifs)
+		{
+			return false;
+		}
+
+		std::string scriptContents;
+		ifs.seekg(0, std::ios::end);
+		scriptContents.resize(static_cast<unsigned int>(ifs.tellg()));
+		ifs.seekg(0, std::ios::beg);
+		ifs.read(&scriptContents[0], scriptContents.size());
+		ifs.close();
+
+		return executeStatements(scriptContents);
+	}
+
+	bool SQLExecutor::executeStatements(const std::string& sqlStatements)
+	{
+		sqlite3* db = NULL;
+		int openResult = sqlite3_open(m_dbFilepath.c_str(), &db);
+		if (openResult != SQLITE_OK)
+		{
+			return false;
+		}
+
+		int execResult = sqlite3_exec(db, sqlStatements.c_str(), NULL, NULL, NULL);
+		if (execResult != SQLITE_OK)
+		{
+			sqlite3_close(db);
+			return false;
+		}
+
+		int closeResult = sqlite3_close(db);
+		if (closeResult != SQLITE_OK)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+}}
---
+++ test/DatabaseIntegrationTest/Tools/SQLExecutor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Commands/ICommand.h"
+
+
+namespace seed_cpp { namespace model {
+	class User;
+}}
+
+namespace seed_cpp { namespace db_test {
+
+	class SQLExecutor
+	{
+	public:
+		SQLExecutor(const std::string& dbFilepath);
+		virtual ~SQLExecutor();
+
+		bool executeScripts(const std::vector<std::string>&);
+		bool executeScript(const std::string&);
+		bool executeStatements(const std::string&);
+
+	private:
+		std::string m_dbFilepath;
+	};
+
+}}
