cbae98328e0484590c5023c53f8b5518905c556b
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-06-11 22:36:51 +0200

    Added test for basic user

--- test/SeedCppCoreUnitTest/Tests/DAL/Translators/Db/UserDbTranslatorTest.cpp
+++ test/SeedCppCoreUnitTest/Tests/DAL/Translators/Db/UserDbTranslatorTest.cpp
@@ -49,10 +49,50 @@ namespace seed_cpp { namespace unit_test {
 		}
 	};
 
+	struct BasicUserWrapper
+	{
+		typedef model::User ModelEntity;
+
+		static model::User getEntity()
+		{
+			model::User user;
+			user.setId(std::string("BB65526F-3110-400F-98E8-77AD289C2175"));
+			user.setLogin("agarcia");
+			user.setPassword("M123456p");
+			user.setName("Alex");
+			user.setSurname("Garcia");
+			user.setRole(model::User::USER_ROLE);
+			user.setCreationTime(boost::posix_time::ptime(boost::gregorian::date(2015, 3, 14),
+														  boost::posix_time::time_duration(8, 1, 2, 345)));
+			user.setUpdateTime(boost::posix_time::ptime(boost::gregorian::date(2018, 5, 19),
+														boost::posix_time::time_duration(1, 2, 3)));
+
+			return user;
+		}
+
+		static std::vector<StubFieldValue> getRecordData()
+		{
+			return	{ StubFieldValue("id", std::string("BB65526F-3110-400F-98E8-77AD289C2175")),
+					  StubFieldValue("login", std::string("agarcia")),
+					  StubFieldValue("password", std::string("M123456p")),
+					  StubFieldValue("name", std::string("Alex")),
+					  StubFieldValue("surname", std::string("Garcia")),
+					  StubFieldValue("role", static_cast<int>(model::User::USER_ROLE)),
+					  StubFieldValue("creationTime", boost::posix_time::from_iso_string("20150314T080102.000345")),
+					  StubFieldValue("updateTime", boost::posix_time::from_iso_string("20180519T010203")),
+			};
+		}
+
+		static std::unique_ptr<dal::UserDbTranslator> buildDbTranslator(model::User& entity)
+		{
+			return std::make_unique<dal::UserDbTranslator>(entity);
+		}
+	};
+
 
 	typedef ::testing::Types<
-		AdminUserWrapper/*,
-		BasicUserWrapper*/
+		AdminUserWrapper,
+		BasicUserWrapper
 	> UserDbTranslatorTestWrappers;
 
 	INSTANTIATE_TYPED_TEST_CASE_P(UserTranslatorTest, DatabaseTranslatorTest, UserDbTranslatorTestWrappers);
