0853a6852f8d604b2f8a188d60ac43fc79a7bd26
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-06-16 13:08:59 +0200

    Fixed compilation

--- test/SeedCppCoreTestUtilities/Mocks/Services/System/MockResourceService.h
+++ test/SeedCppCoreTestUtilities/Mocks/Services/System/MockResourceService.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "SeedCppCore/Services/System/IUUIDGeneratorService.h"
+#include "SeedCppCore/Services/System/IResourceService.h"
 
 
 namespace seed_cpp { namespace test_utility {
 
-	class MockUUIDGeneratorService : public service::IUUIDGeneratorService
+	class MockResourceService : public service::IResourceService
 	{
 	public:
-		MockUUIDGeneratorService();
-		virtual ~MockUUIDGeneratorService();
+		MockResourceService();
+		virtual ~MockResourceService();
 
 		MOCK_CONST_METHOD2(loadResourceAsString, std::string(unsigned int, const std::string&));
 		MOCK_CONST_METHOD2(loadResourceAsString, std::string(const std::string&, const std::string&));
