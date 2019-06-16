3176f352848e105c24d3831ca5accb01790d67f3
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-06-16 12:53:37 +0200

    Added mock for UUID generator service

---
+++ test/SeedCppCoreTestUtilities/Mocks/Services/System/MockUUIDGeneratorService.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "MockUUIDGeneratorService.h"
+
+
+namespace seed_cpp { namespace test_utility {
+
+	MockUUIDGeneratorService::MockUUIDGeneratorService()
+	{
+	}
+
+	MockUUIDGeneratorService::~MockUUIDGeneratorService()
+	{
+	}
+
+}}
