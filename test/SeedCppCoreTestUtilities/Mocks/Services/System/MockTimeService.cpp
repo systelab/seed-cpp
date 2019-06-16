5f12e0af2fbdf4e1ca42f6ef6a1ce0f9a7763e9e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-06-16 12:41:09 +0200

    Added mock for time service

---
+++ test/SeedCppCoreTestUtilities/Mocks/Services/System/MockTimeService.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "MockTimeService.h"
+
+
+namespace seed_cpp { namespace test_utility {
+
+	MockTimeService::MockTimeService()
+	{
+	}
+
+	MockTimeService::~MockTimeService()
+	{
+	}
+
+}}
