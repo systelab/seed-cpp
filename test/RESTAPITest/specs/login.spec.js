414626cdb27c0cff50895480e3e4ae2c73e22557
ehilinger <ehilinger@werfen.com>
2019-06-18 08:37:44 +0200

    First example login test

--- test/RESTAPITest/specs/login.spec.js
+++ test/RESTAPITest/specs/login.spec.js
@@ -1,5 +1,14 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
 var assert = require('assert');
 var child_process = require('child_process');
+
+chai.use(chaiHttp);
+const url= 'http://localhost:8080/seed/v1';
+
+var token;
+
 
 function sleep(ms)
 {
@@ -8,12 +17,11 @@ function sleep(ms)
   })
 }
 
-describe('rest/api/users', () =>
-{
-  describe('GET endpoint returns an array', () => 
-  {
-    xit('should return -1 when the value is not present', async () =>
-    {
+
+describe('Login: ',()=>{
+
+it('should login successfully', async () => {
+
       const serverProcess = child_process.spawn("SeedCpp.exe", ["--port", "7070"], { cwd: "../../build/bin/" }, (err, data) =>
       {  
         console.log(err)
@@ -37,16 +45,23 @@ describe('rest/api/users', () =>
           `server process terminated due to receipt of signal ${signal}`);
       });
 
-      await sleep(5000);
+      await sleep(2000);
+    
+    chai.request(url).post('/users/login').set('content-type', 'application/x-www-form-urlencoded').send({login:'Systelab', password:'Systelab'}).end( async function(err,res)
+    {
+      console.log(res.body)
+      expect(res).to.have.status(200);
+      expect(res.header.authorization).not.equal(null);
+      token = res.header.authorization;
+      console.log('Your token is:', res.header.authorization);
 
-      console.log ("Performing assertion...");
-      assert.equal(-1, [1,2,3].indexOf(4));
-      console.log ("Assertion completed");
-
-      await sleep(10000);
+      await sleep(2000);
 
       serverProcess.kill('SIGTERM');
       console.log("Kill process");
-    });
   });
 });
+
+});
+
+
