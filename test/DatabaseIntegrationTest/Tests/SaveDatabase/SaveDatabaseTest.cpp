f2882d0d950bd98468da07e59275dcadde80de87
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-07-09 18:03:52 +0200

    Registered patient save scenarios

--- test/DatabaseIntegrationTest/Tests/SaveDatabase/SaveDatabaseTest.cpp
+++ test/DatabaseIntegrationTest/Tests/SaveDatabase/SaveDatabaseTest.cpp
@@ -4,6 +4,7 @@
 #include "Tools/Commands/ICommand.h"
 
 #include "DatabaseIntegrationTest/Tests/SaveDatabase/SaveUserScenariosBuilder.h"
+#include "DatabaseIntegrationTest/Tests/SaveDatabase/SavePatientScenariosBuilder.h"
 #include "DatabaseIntegrationTest/Tools/Core.h"
 #include "DatabaseIntegrationTest/Tools/SQLExecutor.h"
 
@@ -61,5 +62,6 @@ namespace seed_cpp { namespace db_test {
 	}
 
 	INSTANTIATE_TEST_CASE_P(SaveUsers, SaveDatabaseTest, ValuesIn(SaveUserScenariosBuilder::build()));
+	INSTANTIATE_TEST_CASE_P(SavePatients, SaveDatabaseTest, ValuesIn(SavePatientScenariosBuilder::build()));
 
 }}
