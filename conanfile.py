49475876c371659e77e74e3d608acd0d2032ad37
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-08-18 18:05:44 +0200

    Moved to Python conanfile

---
+++ conanfile.py
@@ -0,0 +1,31 @@
+from conans import ConanFile, tools
+
+class SeedCppConan(ConanFile):
+    name = "SeedCpp"
+    description = "Seed for Systelab C++ projects"
+    url = "https://github.com/systelab/seed-cpp"
+    homepage = "https://github.com/systelab/seed-cpp"
+    author = "CSW <csw@werfen.com>"
+    topics = ("conan", "seed", "cpp", "systelab", "new", "projects")
+    license = "MIT"
+    generators = "cmake_find_package"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def requirements(self):
+        self.requires("DbSQLiteAdapter/1.1.1@systelab/stable")
+        self.requires("RapidJSONAdapter/1.0.1@systelab/stable")
+        self.requires("BoostAsioWebServerAdapter/1.0.1@systelab/stable")
+        self.requires("JWTUtils/1.0.1@systelab/stable")
+        self.requires("RESTAPICore/1.0.0@systelab/stable")
+
+    def imports(self):
+        self.copy("*.dll", dst=("bin/%s" % self.settings.build_type), src="bin")
+        self.copy("*.dylib*", dst=("bin/%s" % self.settings.build_type), src="lib")
+        self.copy("*.so*", dst=("bin/%s" % self.settings.build_type), src="lib")
+
+    def package(self):
+        self.copy("*.exe", dst="bin", src="bin/SeedCpp")
+        self.copy("*.dll", dst="bin", src="bin/SeedCpp")
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
