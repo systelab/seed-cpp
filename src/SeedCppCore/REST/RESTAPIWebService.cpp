daeac1d8ba6129969e1564906096d0287009d6bc
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-08-19 20:12:31 +0200

    Registered a route for all the existing endpoints

--- src/SeedCppCore/REST/RESTAPIWebService.cpp
+++ src/SeedCppCore/REST/RESTAPIWebService.cpp
@@ -3,6 +3,7 @@
 
 #include "REST/Endpoints/IEndpointsFactory.h"
 
+#include "RESTAPICore/Router/IRoute.h"
 #include "RESTAPICore/Router/Router.h"
 #include "RESTAPICore/Router/RoutesFactory.h"
 #include "RESTAPICore/Endpoint/IEndpoint.h"
@@ -11,41 +12,42 @@
 #include "WebServerAdapterInterface/Model/Request.h"
 
 
+using namespace systelab::rest_api_core;
+
 namespace seed_cpp { namespace rest {
 
 	RESTAPIWebService::RESTAPIWebService(IEndpointsFactory& endpointsFactory)
 		:m_endpointsFactory(endpointsFactory)
 		,m_router(std::make_unique<systelab::rest_api_core::Router>())
+		,m_routesFactory()
 	{
-		//std::string jwtKey = "JWTKey";
-		//systelab::rest_api_core::RoutesFactory routesFactory(jwtKey);
+		std::string jwtKey = "JWTKey";
+		m_routesFactory = std::make_unique<systelab::rest_api_core::RoutesFactory>(jwtKey);
 
-		//routesFactory.buildRoute("GET", "seed/v1/allergies", {}, )
+		// Health
+		addRoute("GET",    "seed/v1/health",            std::bind(&IEndpointsFactory::buildHealthGetEndpoint,       std::ref(m_endpointsFactory)), RouteAccess::ANONYMOUS);
 
-		//// Allergies
-		//m_router->addRoute("GET",    "seed/v1/allergies",         std::bind(&IEndpointsFactory::buildAllergiesGetAllEndpoint, std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("GET",    "seed/v1/allergies/:id",     std::bind(&IEndpointsFactory::buildAllergiesGetEndpoint,    std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("POST",   "seed/v1/allergies/allergy", std::bind(&IEndpointsFactory::buildAllergiesPostEndpoint,   std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("PUT",    "seed/v1/allergies/:id",     std::bind(&IEndpointsFactory::buildAllergiesPutEndpoint,    std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("DELETE", "seed/v1/allergies/:id",     std::bind(&IEndpointsFactory::buildAllergiesDeleteEndpoint, std::ref(m_endpointsFactory), _1));
+		// Allergies
+		addRoute("GET",    "seed/v1/allergies",         std::bind(&IEndpointsFactory::buildAllergiesGetAllEndpoint, std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("GET",    "seed/v1/allergies/:id",     std::bind(&IEndpointsFactory::buildAllergiesGetEndpoint,    std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("POST",   "seed/v1/allergies/allergy", std::bind(&IEndpointsFactory::buildAllergiesPostEndpoint,   std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("PUT",    "seed/v1/allergies/:id",     std::bind(&IEndpointsFactory::buildAllergiesPutEndpoint,    std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("DELETE", "seed/v1/allergies/:id",     std::bind(&IEndpointsFactory::buildAllergiesDeleteEndpoint, std::ref(m_endpointsFactory)), RouteAccess::BASIC);
 
-		//// Health
-		//m_router->addRoute("GET",    "seed/v1/health",            std::bind(&IEndpointsFactory::buildHealthGetEndpoint,      std::ref(m_endpointsFactory), _1));
+		// Patients
+		addRoute("GET",    "seed/v1/patients",          std::bind(&IEndpointsFactory::buildPatientsGetAllEndpoint,  std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("GET",    "seed/v1/patients/:id",      std::bind(&IEndpointsFactory::buildPatientsGetEndpoint,     std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("POST",   "seed/v1/patients/patient",  std::bind(&IEndpointsFactory::buildPatientsPostEndpoint,    std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("PUT",    "seed/v1/patients/:id",      std::bind(&IEndpointsFactory::buildPatientsPutEndpoint,     std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("DELETE", "seed/v1/patients/:id",      std::bind(&IEndpointsFactory::buildPatientsDeleteEndpoint,  std::ref(m_endpointsFactory)), RouteAccess::BASIC);
 
-		//// Patients
-		//m_router->addRoute("GET",    "seed/v1/patients",         std::bind(&IEndpointsFactory::buildPatientsGetAllEndpoint, std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("GET",    "seed/v1/patients/:id",     std::bind(&IEndpointsFactory::buildPatientsGetEndpoint,    std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("POST",   "seed/v1/patients/patient", std::bind(&IEndpointsFactory::buildPatientsPostEndpoint,   std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("PUT",    "seed/v1/patients/:id",     std::bind(&IEndpointsFactory::buildPatientsPutEndpoint,    std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("DELETE", "seed/v1/patients/:id",     std::bind(&IEndpointsFactory::buildPatientsDeleteEndpoint, std::ref(m_endpointsFactory), _1));
-
-		//// Users
-		//m_router->addRoute("GET",    "seed/v1/users",            std::bind(&IEndpointsFactory::buildUsersGetAllEndpoint,    std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("GET",    "seed/v1/users/:id",        std::bind(&IEndpointsFactory::buildUsersGetEndpoint,       std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("POST",   "seed/v1/users/user",       std::bind(&IEndpointsFactory::buildUsersPostEndpoint,      std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("PUT",    "seed/v1/users/:id",        std::bind(&IEndpointsFactory::buildUsersPutEndpoint,       std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("DELETE", "seed/v1/users/:id",        std::bind(&IEndpointsFactory::buildUsersDeleteEndpoint,    std::ref(m_endpointsFactory), _1));
-		//m_router->addRoute("POST",   "seed/v1/users/login",      std::bind(&IEndpointsFactory::buildUsersLoginPostEndpoint, std::ref(m_endpointsFactory), _1));
+		// Users
+		addRoute("GET",    "seed/v1/users",             std::bind(&IEndpointsFactory::buildUsersGetAllEndpoint,     std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("GET",    "seed/v1/users/:id",         std::bind(&IEndpointsFactory::buildUsersGetEndpoint,        std::ref(m_endpointsFactory)), RouteAccess::BASIC);
+		addRoute("POST",   "seed/v1/users/user",        std::bind(&IEndpointsFactory::buildUsersPostEndpoint,       std::ref(m_endpointsFactory)), RouteAccess::ADMIN);
+		addRoute("PUT",    "seed/v1/users/:id",         std::bind(&IEndpointsFactory::buildUsersPutEndpoint,        std::ref(m_endpointsFactory)), RouteAccess::ADMIN);
+		addRoute("DELETE", "seed/v1/users/:id",         std::bind(&IEndpointsFactory::buildUsersDeleteEndpoint,     std::ref(m_endpointsFactory)), RouteAccess::ADMIN);
+		addRoute("POST",   "seed/v1/users/login",       std::bind(&IEndpointsFactory::buildUsersLoginPostEndpoint,  std::ref(m_endpointsFactory)), RouteAccess::ADMIN);
 	}
 
 	RESTAPIWebService::~RESTAPIWebService() = default;
@@ -64,6 +66,15 @@ namespace seed_cpp { namespace rest {
 		{
 			return m_router->process(request);
 		}
+	}
+
+	void RESTAPIWebService::addRoute(const std::string& method,
+									 const std::string& uri,
+									 const std::function<std::unique_ptr<IEndpoint>()> endpointFactoryMethod,
+									 RouteAccess /*access*/) const
+	{
+		auto route = m_routesFactory->buildRoute(method, uri, {}, endpointFactoryMethod);
+		m_router->addRoute(std::move(route));
 	}
 
 }}
--- src/SeedCppCore/REST/RESTAPIWebService.h
+++ src/SeedCppCore/REST/RESTAPIWebService.h
@@ -3,7 +3,9 @@
 #include "WebServerAdapterInterface/IWebService.h"
 
 namespace systelab { namespace rest_api_core {
+	class IEndpoint;
 	class Router;
+	class RoutesFactory;
 }}
 
 namespace seed_cpp { namespace rest {
@@ -16,11 +18,25 @@ namespace seed_cpp { namespace rest {
 		RESTAPIWebService(IEndpointsFactory&);
 		virtual ~RESTAPIWebService();
 
-		std::unique_ptr<systelab::web_server::Reply> process(const systelab::web_server::Request& request) const;
+		std::unique_ptr<systelab::web_server::Reply> process(const systelab::web_server::Request&) const;
+
+	private:
+		enum class RouteAccess
+		{
+			ANONYMOUS = 0,
+			BASIC = 1,
+			ADMIN = 2
+		};
+
+		void addRoute(const std::string& method,
+					  const std::string& uri,
+					  const std::function<std::unique_ptr<systelab::rest_api_core::IEndpoint>()> endpointFactoryMethod,
+					  RouteAccess access) const;
 
 	private:
 		IEndpointsFactory& m_endpointsFactory;
 		std::unique_ptr<systelab::rest_api_core::Router> m_router;
+		std::unique_ptr<systelab::rest_api_core::RoutesFactory> m_routesFactory;
 	};
 
 }}
