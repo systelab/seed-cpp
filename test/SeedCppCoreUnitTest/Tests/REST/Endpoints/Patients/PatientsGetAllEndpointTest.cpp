ae9a5c80f10e6d8c78e5146e615219230c03b785
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-08-22 23:36:10 +0200

    Added test instance for patients get all endpoint

---
+++ test/SeedCppCoreUnitTest/Tests/REST/Endpoints/Patients/PatientsGetAllEndpointTest.cpp
@@ -0,0 +1,77 @@
+#include "stdafx.h"
+#include "SeedCppCoreUnitTest/Tests/REST/Endpoints/EntityGetAllEndpointTest.h"
+
+#include "SeedCppCore/Model/Patient.h"
+#include "SeedCppCore/Model/PatientMgr.h"
+#include "SeedCppCore/REST/Endpoints/Patients/PatientsGetAllEndpoint.h"
+
+#include "SeedCppCoreTestUtilities/Mocks/DAL/Translators/JSON/MockJSONSaveTranslator.h"
+
+
+using namespace seed_cpp::test_utility;
+using namespace testing;
+
+namespace seed_cpp { namespace unit_test {
+
+	struct PatientsGetAllEndpointBaseWrapper
+	{
+		typedef model::Patient ModelEntity;
+		typedef model::PatientMgr ModelManager;
+		typedef rest::PatientsGetAllEndpoint Endpoint;
+
+		static void setUpJSONTranslatorsFactory(MockJSONTranslatorsFactory& jsonTranslatorsFactory)
+		{
+			ON_CALL(jsonTranslatorsFactory, buildPatientSaveTranslatorProxy(_)).WillByDefault(Invoke(
+				[](const ModelEntity& entityValue) -> dal::IJSONSaveTranslator*
+				{
+					auto jsonTranslator = std::make_unique<MockJSONSaveTranslator>();
+					EXPECT_CALL(*jsonTranslator, saveEntityToJSON(_)).WillOnce(Invoke(
+						[entityValue](systelab::json::IJSONValue& jsonValue) -> void
+						{
+							jsonValue.addMember("id", *entityValue.getId());
+						}
+					));
+
+					return jsonTranslator.release();
+				}
+			));
+		}
+
+		static void tearDownWrapper()
+		{
+		}
+	};
+
+	struct PatientsGetAllEndpointEmptyWrapper : public PatientsGetAllEndpointBaseWrapper
+	{
+		static std::vector<model::Patient> getEntities()
+		{
+			return {};
+		}
+
+		static std::string getExpectedReplyContent()
+		{
+			std::stringstream ss;
+			ss << "{" << std::endl;
+			ss << "   \"content\": []," << std::endl;
+			ss << "   \"totalElements\": 0," << std::endl;
+			ss << "   \"first\": true," << std::endl;
+			ss << "   \"last\": false," << std::endl;
+			ss << "   \"number\": 0," << std::endl;
+			ss << "   \"numberOfElements\": 0," << std::endl;
+			ss << "   \"size\": 20," << std::endl;
+			ss << "   \"totalPages\": 0" << std::endl;
+			ss << "}" << std::endl;
+
+			return ss.str();
+		}
+	};
+
+
+	typedef ::testing::Types<
+		PatientsGetAllEndpointEmptyWrapper
+	> PatientsGetAllEndpointWrappers;
+
+	INSTANTIATE_TYPED_TEST_CASE_P(PatientsGetAllEndpointTest, EntityGetAllEndpointTest, PatientsGetAllEndpointWrappers);
+
+}}
