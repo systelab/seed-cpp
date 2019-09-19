cd9b87fccab06a1368a34eaf5f3b9e364cc2ed63
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-09-19 20:42:05 +0200

    Add mock for allergies model entity

---
+++ test/SeedCppCoreTestUtilities/Mocks/Services/Model/MockAllergyModelService.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "MockAllergyModelService.h"
+
+
+namespace seed_cpp { namespace test_utility {
+
+	MockAllergyModelService::MockAllergyModelService()
+	{
+	}
+
+	MockAllergyModelService::~MockAllergyModelService()
+	{
+	}
+
+}}
