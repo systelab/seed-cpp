a9ea760ea42ddc0fccb33d2d25460e0653c9d475
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-09-19 21:16:46 +0200

    Fixed includes

--- src/SeedCppCore/REST/Endpoints/Users/UsersDeleteEndpoint.h
+++ src/SeedCppCore/REST/Endpoints/Users/UsersDeleteEndpoint.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "REST/Endpoints/EntityDeleteEndpoint.h"
-#include "Services/Model/IUserModelService.h"
+#include "SeedCppCore/REST/Endpoints/EntityDeleteEndpoint.h"
+#include "SeedCppCore/Services/Model/IUserModelService.h"
+
 
 namespace seed_cpp { namespace rest {
 
