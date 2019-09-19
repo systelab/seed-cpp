3474ec2e72871b1657c779a3a124740e1290e074
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-09-19 20:04:14 +0200

    Grouped methods

--- src/SeedCppCore/Services/Model/IPatientModelService.h
+++ src/SeedCppCore/Services/Model/IPatientModelService.h
@@ -17,6 +17,7 @@ namespace seed_cpp { namespace service {
 
 		virtual model::PatientMgr& getEntityMgr() const = 0;
 		virtual const model::Patient* getEntityById(const std::string& id, const model::LockableEntityMgrSubject::IReadLock&) const = 0;
+
 		virtual const model::Patient& addEntity(std::unique_ptr<model::Patient>, const model::LockableEntityMgrSubject::IWriteLock&) = 0;
 		virtual const model::Patient& editEntity(std::unique_ptr<model::Patient>, const model::LockableEntityMgrSubject::IWriteLock&) = 0;
 		virtual void deleteEntity(const std::string& id, const model::LockableEntityMgrSubject::IWriteLock&) = 0;
