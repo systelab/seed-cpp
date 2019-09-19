3f0dc26ca2a1500b390fb76bceaebe8e11264c15
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-09-19 20:41:52 +0200

    Fixed includes

--- src/SeedCppCore/REST/Endpoints/Allergies/AllergiesDeleteEndpoint.h
+++ src/SeedCppCore/REST/Endpoints/Allergies/AllergiesDeleteEndpoint.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "REST/Endpoints/EntityDeleteEndpoint.h"
-#include "Services/Model/IAllergyModelService.h"
+#include "SeedCppCore/REST/Endpoints/EntityDeleteEndpoint.h"
+#include "SeedCppCore/Services/Model/IAllergyModelService.h"
 
 
 namespace seed_cpp { namespace rest {
