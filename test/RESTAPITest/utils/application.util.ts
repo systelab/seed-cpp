a40b8e71b5093c25c773956071ac4a4f2547de4a
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-29 19:31:33 +0100

    Added trace when application process not working

--- test/RESTAPITest/utils/application.util.ts
+++ test/RESTAPITest/utils/application.util.ts
@@ -8,6 +8,11 @@ export class Application
     public constructor(name: string, args: string[], workingDir: string)
     {
         this.applicationProcess = child_process.spawn(name, args, { cwd: workingDir });
+        this.applicationProcess.on("error", function(err)
+        {
+            console.error(`Error while executing '${name}' application on '${workingDir}' folder:`);
+            console.error("Error details:", err);
+        });
     }
 
     public close(): void
